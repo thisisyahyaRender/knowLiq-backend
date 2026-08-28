e3f700db50f1e693bc02ed306e20b2feec39c2cc
thisisyahya <themultiblogsmania@gmail.com>
2026-08-28 15:41:24 +0500

    added firebase-secrets to gitignore

--- app.js
+++ app.js
@@ -11,7 +11,22 @@ var admin = require('firebase-admin');
 // --- 1. FIREBASE ADMIN SETUP ---
 // Make sure firebase-secrets.json is saved in the same folder as this app.js
 const { initializeApp, cert } = require('firebase-admin/app');
-const serviceAccount = require('./firebase-secrets.json');
+
+
+const serviceAccount = {
+  type: process.env.TYPE || "service_account",
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI || "https://accounts.google.com/o/oauth2/auth",
+  token_uri: process.env.TOKEN_URI || "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL || "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN || "googleapis.com"
+};
+
 initializeApp({
   credential: cert(serviceAccount)
 });
